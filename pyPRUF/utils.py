fb468fcaf2a0af5021c8d01aefb8cacab8fc2c50
frasga <s.francesco297@gmail.com>
2025-08-03 13:26:32 +0200

    ref(main): Add requirements.txt and fix utils.py documentation

--- pyPRUF/utils.py
+++ pyPRUF/utils.py
@@ -1,10 +1,61 @@
+from numbers import Number
 import numpy as np
 
-def is_list_out_of_range(x, inf, sup):
+def is_list_out_of_range(x: list, inf: Number, sup: Number):
+    """
+    Check if one element of a list is not in a range
+
+    Parameters
+    ----------
+    x: list
+        input list that will be checked to check
+    inf: Number
+        upper range bound
+    sup: Number
+        lower range bound
+
+    Returns
+    -------
+    bool
+        True if one element of x is out of range
+        False otherwise
+    """
     return any(is_out_of_range(elem, inf, sup) for elem in x)
 
-def is_out_of_range(x, inf, sup):
+def is_out_of_range(x: Number, inf: Number, sup: Number) -> bool:
+    """
+    Check if element is not in a range
+
+    Parameters
+    ----------
+    x: Number
+        element to check
+    inf: Number
+        upper range bound
+    sup: Number
+        lower range bound
+
+    Returns
+    -------
+    bool
+        True if x is out of range
+        False otherwise
+    """
     return inf > x or x > sup
 
-def is_list_not_unique(x):
+def is_list_not_unique(x: list) -> bool:
+    """
+    Check if list contains duplicates
+
+    Parameters
+    ----------
+    x: list
+        Input list that contains elements
+
+    Returns
+    -------
+    bool
+        True if list contains duplicate
+        False otherwise
+    """
     return np.unique(x).size != len(x)
